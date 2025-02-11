6df722fadae2e4e66a88e945f3791541f22143aa
remi.allen67@gmail.com <remi.allen67@gmail.com>
2025-02-11 09:40:19 +0100

    tests

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "point.hpp"
-#include "obstacle.hpp"
+#include "cercle.hpp"
 #include "segment.hpp"
 using namespace std;
 
@@ -9,13 +9,16 @@ int main(){
     Point A(1,1);
     Point C(1,2);
     cout<<"A =("<<A.x<<","<<A.y<<")"<<endl;
-    Obstacle B;
+    Cercle B;
     cout<<"obstacle point "<<B.centre.x<<","<<B.centre.y<<endl;
     cout<<" obstacle rayon = "<<B.rayon<<endl;
 
     Segment S(A,C);
     cout<<"segment P1"<<S.P1.x<<","<<S.P1.y<<endl;
     cout<<"segment P2"<<S.P2.x<<","<<S.P2.y<<endl;
+
+    cout<<"A ="<<A<<endl;
+
 
     return 0;
 }
