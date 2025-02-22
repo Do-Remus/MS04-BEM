d01e06b38c8dce1e53ca6ca2d29626c259c4fbd0
remi.allen67@gmail.com <remi.allen67@gmail.com>
2025-02-22 13:31:28 +0100

    ajout classe Matrice, MatriceSym, fonctions decomposition LDL' et fonction resolution systeme lineaire

--- matrice.hpp
+++ matrice.hpp
@@ -9,16 +9,15 @@ using namespace std;
 
 
 typedef vector<double> Vecteur;
-
-class MatriceSym
-{private :
+class Matrice
+{protected :
     Vecteur coefs;                       // coefficients rangés par ligne
 public:
-    int n;                               // ordre de la MatriceSym
-    MatriceSym (int m=0, double v=0.);      // constructeur dimensions et coefs constants
-    MatriceSym (const Vecteur& d);          // constructeur d'une MatriceSym diagonale
-    MatriceSym (const initializer_list<Vecteur>& vs); // constructeur d'une MatriceSym à partir de vecteurs
-    double& coef(int i,int j) ;          // acces au coef i,j (1->n)
+    int n;                               // ordre de la Matrice
+    Matrice (int m=0, double v=0.);
+    Matrice (const Vecteur& d);
+    Matrice(const initializer_list<Vecteur>& vs); // constructeur d'une MatriceSym à partir de vecteurs
+    double& coef(int i,int j) ;         // acces au coef i,j (1->n)
     double  coef(int i, int j) const ;   // acces au coef i,j (1->n) const
     double& operator() (int i, int j);
     double operator() (int i,int j) const;
@@ -27,20 +26,31 @@ public:
 
 };
 
-MatriceSym::MatriceSym (int m, double v)      // constructeur dimensions et coefs constants
+class MatriceSym : public Matrice
+{public:
+    MatriceSym(int m=0, double v=0.);
+};
+
+
+
+Matrice::Matrice (int m, double v)      // constructeur dimensions et coefs constants
 {
     n=max(m,0);
     if(m==0) {return;}
     coefs.resize(n*n,v);
 }
-MatriceSym::MatriceSym (const Vecteur& d)     // constructeur d'une MatriceSym diagonale
+
+
+
+
+Matrice::Matrice (const Vecteur& d)     // constructeur d'une MatriceSym diagonale
 {
     n=d.size();
     coefs.resize(n*n,0.);
     for(int i=0;i<n;i++)
         coefs[i*n+i]=d[i];
 }
-MatriceSym::MatriceSym(const initializer_list<Vecteur>& vs) // constructeur à partir de vecteurs
+Matrice::Matrice(const initializer_list<Vecteur>& vs) // constructeur à partir de vecteurs
 {
     n=vs.size();
     coefs.resize(n*n);
@@ -60,13 +70,15 @@ MatriceSym::MatriceSym(const initializer_list<Vecteur>& vs) // constructeur à p
     }
     
 }
-double& MatriceSym::coef(int i,int j)       // acces au coef i,j (1->n)
+double& Matrice::coef(int i,int j)       // acces au coef i,j (1->n)
 {
      if(i<=0 || j<=0) {cout<<"coef(i,j) : i,j en dehors des bornes"<<endl; exit(-1);}
      return coefs[(i-1)*n+j-1];
 }
 
-double& MatriceSym::operator() (int i, int j){
+
+
+double& Matrice::operator() (int i, int j){
       if(i<=0 || j<=0 || j>=n || i >=this->n) {cout<<"coef(i,j) : i,j en dehors des bornes"<<endl; exit(-1);}
      return this->coefs[(i-1)*this->n+j-1];
 }
@@ -74,17 +86,106 @@ double& MatriceSym::operator() (int i, int j){
 
 
 
-double  MatriceSym::coef(int i, int j) const // acces au coef i,j (1->n) const
+double  Matrice::coef(int i, int j) const // acces au coef i,j (1->n) const
 {
      if(i<=0 || j<=0) {cout<<"coef(i,j) : i,j en dehors des bornes"<<endl; exit(-1);}
      return coefs[(i-1)*n+j-1];
 }
 
-double MatriceSym::operator() (int i, int j) const {
+double Matrice::operator() (int i, int j) const {
       if(i<=0 || j<=0 || j>=n || i >=this->n) {cout<<"coef(i,j) : i,j en dehors des bornes"<<endl; exit(-1);}
      return this->coefs[(i-1)*this->n+j-1];
 
 }
+
+
+
+//MatriceSym
+
+MatriceSym::MatriceSym (int m, double v)      // constructeur dimensions et coefs constants
+{
+    n=max(m,0);
+    if(m==0) {return;}
+    coefs.resize(n*(n+1)/2,v); //reduction du nombre de coefs necessaires pour definir la matrice
+}
+
+
+
+//factorisation LDL
+void decomposition_LDL(const MatriceSym& A, Matrice& L, Vecteur& D) {
+    // init matrice L et D
+    int n=A.n;
+    L.n=n;
+    D.resize(n);
+
+    for (int i = 0; i <n; ++i) {
+        // calcul D[i]
+        D[i] = A(i,i);
+        for (int k = 0; k < i; ++k) {
+            D[i] -= L(i,k) * L(i,k) * D[i];
+        }
+
+        // Ensure matrix is positive definite
+        if (D[i] == 0) {
+            cout << "Matrix is not positive definite!" << endl;
+            exit(-1);
+        }
+
+        // Compute L[i][j] for j < i
+        for (int j = i + 1; j < n; ++j) {
+            L(j,i) = A(j,i);
+            for (int k = 0; k < i; ++k) {
+                L(j,i) -= L(j,k) * L(i,k) * D[k];
+            }
+            L(j,i) /= D[i];
+        }
+
+        // Set diagonal elements of L to 1
+        L(i,i) = 1.0;
+    }
+
+    return;
+}
+
+Vecteur resolution_systeme_lineaire(const MatriceSym& A, const Vecteur& P){
+    //va resoudre pour Q AQ=P en 3 parties 
+    //1 -- A=LDL'
+    //2 -- resolution LY=P pour Y
+    //3 -- resolution DL'Q=Y
+
+    int n=A.n;
+    //decomposition de A=LDL'
+    Matrice L(n, 0);
+    Vecteur D(n); //represente la matrice diagonale D
+    decomposition_LDL(A,L,D);
+
+    //resolution LY=P pour Y par recurrence simple grace a la forme finale de la methode de Gauss du systeme lineaire
+    //(substition)
+    Vecteur Y(n);
+    //initialisation
+    Y[1]=P[1];
+    //heredite
+    for(int i=2;i<n;i++){
+        Y[i]+=P[i];
+        for(int k=1;i<k-1;k++){
+            Y[i]-=L(i,k)*Y[k];
+        }
+    }
+
+    //resolution DL'Q=Y : recurrence simple decroissante sur l'indice
+    Vecteur Q(n);
+    //initialisation
+    Q[1]=Y[1]/D[1];
+    //heredite
+    for(int i=n;i>0;i--){
+        Q[i]+=Y[i]/D[i];
+        for(int k=i+1;k<n;k++){
+            Q[i]-=L(i,k)*Q[k];
+        }
+    }
+    return Q;
+}
+
 
 
 
