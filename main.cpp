1d9d814dec92012a372c01c56c880c74138ddf8c
ThomasCreuset <thomas.creuset.pro@gmail.com>
2025-03-04 09:57:12 +0100

    [Thomas] Retrait commentaires

--- main.cpp
+++ main.cpp
@@ -32,9 +32,7 @@ int main()
 
     Maillage monMaillage = genere_maillage_couche_diffusante(5, 10, 5, 0.01);
 
-    cout << "la" << endl;
     monMaillage.export_maillage("output.txt");
-    cout << "la" << endl;
 
     return 0;
 }
