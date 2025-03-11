43d3c98fd051d23b739cd472cba3b7a34c65b504
ThomasCreuset <thomas.creuset.pro@gmail.com>
2025-03-11 09:17:25 +0100

    [Thomas] Resolution

--- main.cpp
+++ main.cpp
@@ -17,25 +17,25 @@ double f(const Point &A)
 int main()
 {
     srand(time(NULL));
-    Point A(1, 1);
+    Point M(1, 1);
     Point C(1, 2);
-    cout << "A =(" << A.x << "," << A.y << ")" << endl;
+    cout << "A =(" << M.x << "," << M.y << ")" << endl;
     Cercle B;
     cout << "obstacle point " << B.centre.x << "," << B.centre.y << endl;
     cout << " obstacle rayon = " << B.rayon << endl;
 
-    Segment S(A, C);
+    Segment S(M, C);
     cout << "segment P1" << S.P1.x << "," << S.P1.y << endl;
     cout << "segment P2" << S.P2.x << "," << S.P2.y << endl;
     // test ostream function
-    cout << "A =" << A << endl;
+    cout << "A =" << M << endl;
     // test operations
-    cout << "A+C" << A + C;
-    cout << "A-C" << A - C;
+    cout << "M+C" << M + C;
+    cout << "M-C" << M - C;
     cout << "3*C" << 3 * C;
     cout << "C/5" << C / 5;
     cout << "norme C" << C.norm() << endl;
-    cout << "produit vectoriel composante z =" << A * C;
+    cout << "produit vectoriel composante z =" << M * C;
 
     Maillage monMaillage = genere_maillage_couche_diffusante(5, 10, 5, 0.01);
 
@@ -57,5 +57,16 @@ int main()
     // test MatriceSym
 
     // test decomposition LDL
+
+    // programme
+    double h = 10.;
+    double e = 2.;
+    double pas = 0.001;
+    Maillage maillageFinale = genere_maillage_couche_diffusante(1, h, e, pas);
+    MatriceSym A;
+    Vecteur P;
+    genere_coefficient_matrice_A(A, maillageFinale, pas);
+    genere_coefficient_vecteur_P(P, maillageFinale, pas);
+    Vecteur solution = resolution_systeme_lineaire(A, P);
     return 0;
 }
