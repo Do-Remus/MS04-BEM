48f06e2db75079fa4cf8d2aab8f527016c39a207
Thomas Creuset <thomas.creuset.pro@gmail.com>
2026-09-25 08:08:25 +0200

    Switch back to linux/mac only

--- main.cpp
+++ main.cpp
@@ -44,19 +44,19 @@ int main()
 
         // tests Maillage
         vector<Cercle> obstaclesTest;
-        Point O(0,0);
-        double a= 1.;
+        Point O(0, 0);
+        double a = 1.;
         Cercle Cercle0(a, O);
         obstaclesTest.push_back(Cercle0);
         Maillage monMaillage;
-        cout<< " k= "<<k<< ", a ="<<a<<" N ="<<N<< endl;
-        monMaillage.ajoute_cercle(pasMaillage, Cercle0 );
+        cout << " k= " << k << ", a =" << a << " N =" << N << endl;
+        monMaillage.ajoute_cercle(pasMaillage, Cercle0);
         monMaillage.export_maillage("outputs/test.txt");
-        cout<<"exported maillage"<<endl;
-        //for (unsigned int i = 0; i < obstaclesTest.size(); i++)
+        cout << "exported maillage" << endl;
+        // for (unsigned int i = 0; i < obstaclesTest.size(); i++)
         //{
-        //    cout << "Cercle " << i << ": r = " << obstaclesTest[i].rayon << " centre = " << obstaclesTest[i].centre;
-        //}
+        //     cout << "Cercle " << i << ": r = " << obstaclesTest[i].rayon << " centre = " << obstaclesTest[i].centre;
+        // }
 
         /*
         // tests sommes partielles U_n^+ , q et p
@@ -90,12 +90,12 @@ int main()
             std::string filename_solExt = "outputs/solExtResu_k=" + std::to_string(k) + "_a=" + std::to_string(a) + "_theta=" + std::to_string(theta) + "_N=" + std::to_string(N) +  ".txt";
             complex<double> q_approche = q(P, N,filename_q);
             complex<double> p_approche = p(P, N, filename_p);
-            
+
             complex<double> sol_ext= u_N_plus(P, a, N, filename_solExt);
             qC << P << " "<< theta<<" "<< q_approche.real() << " "<< q_approche.imag()<<endl;
             pC << P << " "<< theta<<" "<< p_approche.real() << " "<< p_approche.imag()<<endl;
         }
-    
+
         double err = 0;
         for (int t = 0; t < 10; t++) {
         double th = M_PI * t / 9.0;
@@ -106,7 +106,7 @@ int main()
         }
         cout << "N=" << N << " boundary error = " << err << endl;
 
-    
+
 
     const string sol_externe = "outputs/u_N_plusResu_k=" + std::to_string(k) + "_a=" + std::to_string(a) +  "_N=" + std::to_string(N) + ".txt";
     export_obsctacles(cheminFichierObstacles,obstaclesTest );
@@ -117,34 +117,31 @@ int main()
         export_fd_q_p("outputs/qFD_k=" + std::to_string(k) + "_a=" + std::to_string(a) + "_N=" + std::to_string(N) + "_h=" +std::to_string(Hdiff) + ".txt",
               "outputs/pFD_k=" + std::to_string(k) + "_a=" + std::to_string(a) + "_N=" + std::to_string(N) + "_h=" +std::to_string(Hdiff)+ ".txt",
               a, N, 50, Hdiff);
-    }
-    */
 
-    //test des quadratures (calculs d'intégrales)
+ */
+    }
+
+    // test des quadratures (calculs d'intégrales)
     fun_double func = [](const double &x) -> complex<double>
-        {
-           return 3*x + x*x + 2 - x*x*x*x;
-        };
-    
+    {
+        return 3 * x + x * x + 2 - x * x * x * x;
+    };
+
     fun_double x2 = [](const double &x) -> complex<double>
-        {
-           return  x*x;
-        };
+    {
+        return x * x;
+    };
 
     complex<double> result = integ_simple_segsimple(func);
-    cout<<"value ="<< result<<"error ="<<result - 14./3. + 2./5.<<endl;
-    
-    complex<double> result1 = integ_simple_segsimple_n(func,2);
-    complex<double> result2 = integ_simple_segsimple_n(func,4);
-    cout<<"for integ_n, n=2 value ="<< result1<<"error ="<<result1 - 14./3. + 2./5.<<endl;
-    cout<<"for integ_n, n=4 value ="<< result2<<"error ="<<result2 - 14./3. + 2./5.<<endl;
+    cout << "value =" << result << "error =" << result - 14. / 3. + 2. / 5. << endl;
+
+    complex<double> result1 = integ_simple_segsimple_n(func, 2);
+    complex<double> result2 = integ_simple_segsimple_n(func, 4);
+    cout << "for integ_n, n=2 value =" << result1 << "error =" << result1 - 14. / 3. + 2. / 5. << endl;
+    cout << "for integ_n, n=4 value =" << result2 << "error =" << result2 - 14. / 3. + 2. / 5. << endl;
 
     complex<double> result3 = integ_simple_segment_ab_n(x2, 2, 0, 4);
-    cout<<"x2 btw 0 and 4 ="<<result3<<endl;
-}
-    
-
-   
+    cout << "x2 btw 0 and 4 =" << result3 << endl;
 
     return 0;
 }
